45ba08443e9cd79806497f698f94ebfb1be412b7
Ababu Alemu <63737795+ababuu@users.noreply.github.com>
2023-08-01 22:00:45 +0300

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,31 +1,25 @@
 class Solution
 {
-  select(arr,i){
+ select(arr,i){
      // code here such that selectionSort() sorts arr[]
-     let min=arr[i];
-     let index;
-     for( ; i<arr.length;i++){ 
-         if(arr[i]<=min){ 
-             min=arr[i]; 
-             index=i;
-         } 
-     } 
-     return index;
+     let selected=i;
+     for(let j=i+1;j<arr.length;j++){
+         if(arr[j]<arr[selected]){
+             selected=j;
+         }
+     }
+     return selected;
   }
 
   //Function to sort the array using selection sort algorithm.
   selectionSort(arr,n){
    //code here
-   let index;
-   let temp;
-   let min;
-    for(let i=0; i<n; i++){ 
-        index=this.select(arr,i);
-        min=arr[index];
-        temp=arr[i];
-        arr[i]=min;
-        arr[index]=temp;
+   for(let i=0;i<n;i++){
+       let selectedIndex=this.select(arr,i);
+       if(selectedIndex!==i){
+           [arr[selectedIndex],arr[i]]=[arr[i],arr[selectedIndex]];
+       }
+        
+   }
   }
-  return arr;
- } 
 }
